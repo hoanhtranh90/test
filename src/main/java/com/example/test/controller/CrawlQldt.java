516dbf42e8d64eb9f6104e8c428f364350578a04
sangol <nguyenkhacsang100@gmail.com>
2020-11-07 13:21:37 +0700

    addssss

--- src/main/java/com/example/test/controller/CrawlQldt.java
+++ src/main/java/com/example/test/controller/CrawlQldt.java
@@ -7,16 +7,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 @RequestMapping("/api/qldt/")
 @RestController
@@ -27,8 +21,9 @@ public class CrawlQldt {
 
 
 
-    @GetMapping("/login")
-    public ResponseEntity<?> saveCookies(
+    @PostMapping("/login")
+    public ResponseEntity<?> saveCookies(@RequestParam(value = "user") String username,
+                                         @RequestParam(value = "pass") String password
 
     ) throws IOException {
 
@@ -46,8 +41,8 @@ public class CrawlQldt {
 //        System.out.println(viewState.attr("value"));
         Connection.Response response = Jsoup.connect(url1)
                 .method(Connection.Method.POST)
-                .data("ctl00$ContentPlaceHolder1$ctl00$ucDangNhap$txtTaiKhoa", "B19DCPT189")
-                .data("ctl00$ContentPlaceHolder1$ctl00$ucDangNhap$txtMatKhau", "longsangBn1")
+                .data("ctl00$ContentPlaceHolder1$ctl00$ucDangNhap$txtTaiKhoa", username)
+                .data("ctl00$ContentPlaceHolder1$ctl00$ucDangNhap$txtMatKhau", password)
                 .data("ctl00$ContentPlaceHolder1$ctl00$ucDangNhap$btnDangNhap", "Đăng Nhập")
                 .data("__EVENTTARGET", "")
                 .data("__EVENTARGUMENT", "")
@@ -69,12 +64,63 @@ public class CrawlQldt {
                 monHocTrongNgay.add(sizeKip.select(monTrongTiet).get(j).text());
             }
             for(int j = 1;j<=6;j++){
-                kipTrongNgay.put("kíp"+j,monHocTrongNgay.get(j-1));
+                kipTrongNgay.put(String.valueOf(j),monHocTrongNgay.get(j-1));
             }
-            listAll.put("thứ " + i, kipTrongNgay);
+            listAll.put(String.valueOf(i), kipTrongNgay);
         }
         Map<String, Map<String, Map<String, String>>> listDayInWeek = new HashMap<>();
         listDayInWeek.put("tkb",listAll);
+
+//        Element docs = homePage.getElementById("ctl00_ContentPlaceHolder1_ctl00_pnlTuan");
+//        Elements sizeKip = docs.select("tbody").first().children();
+//        List<List<List<TreeMap<String,String>>>> listAll = new ArrayList<>(); //treemap tứ sắp xếp
+//        for (int i = 2 ; i < 7;i++) { // thứ trong tuần
+//            List<TreeMap<String,String>> kipTrongNgay = new ArrayList<>();
+//            String monTrongTiet = String.format("td:nth-child(%d)",i);
+//            ArrayList<String> monHocTrongNgay = new ArrayList<>();
+//            for(int j = 0 ; j<=11;j++){
+//                if(j%2 == 0)
+//                    monHocTrongNgay.add(sizeKip.select(monTrongTiet).get(j).text());
+//            }
+//            for(int j = 1;j<=6;j++){
+//                TreeMap<String,String> datatest = new TreeMap<>();
+//                datatest.put("kíp"+j,monHocTrongNgay.get(j-1));
+//                kipTrongNgay.add(datatest);
+//            }
+////            kipTrongNgay.add(datatest);
+//
+//            List<List<TreeMap<String, String>>> dataTamthoi = new ArrayList<>();
+//            dataTamthoi.add(kipTrongNgay);
+//            listAll.add(dataTamthoi);
+//        }
+////        Map<String, Map<String, Map<String, String>>> listDayInWeek = new HashMap<>();
+//        Map<String, List<List<List<TreeMap<String, String>>>>> listDayInWeek = new HashMap<>();
+//        listDayInWeek.put("tkb",listAll);
+
+
+//
+//        Element docs = homePage.getElementById("ctl00_ContentPlaceHolder1_ctl00_pnlTuan");
+//        Elements sizeKip = docs.select("tbody").first().children();
+//        List<TreeMap<String,List<TreeMap<String,String>>>> listAll = new ArrayList<>(); //treemap tứ sắp xếp
+//        for (int i = 2 ; i < 7;i++) { // thứ trong tuần
+//            List<TreeMap<String,String>> kipTrongNgay = new ArrayList<>();
+//            String monTrongTiet = String.format("td:nth-child(%d)",i);
+//            ArrayList<String> monHocTrongNgay = new ArrayList<>();
+//            for(int j = 0 ; j<=11;j++){
+//                if(j%2 == 0)
+//                    monHocTrongNgay.add(sizeKip.select(monTrongTiet).get(j).text());
+//            }
+//            for(int j = 1;j<=6;j++){
+//                TreeMap<String,String> tess = new TreeMap<>();
+//                tess.put(String.valueOf(j),monHocTrongNgay.get(j-1));
+//                kipTrongNgay.add(tess);
+//            }
+//            TreeMap<String,List<TreeMap<String,String>>> dax = new TreeMap<>();
+//            dax.put(String.valueOf(i), kipTrongNgay);
+//            listAll.add(dax);
+//        }
+//        Map<String, List<TreeMap<String, List<TreeMap<String, String>>>>> listDayInWeek = new HashMap<>();
+//        listDayInWeek.put("tkb",listAll);
 
         return ResponseEntity.ok(listDayInWeek);
     }
